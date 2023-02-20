9f9b646b886df6370bfff8b9e12a4106bea861c6
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-02-20 17:04:18 +0900

    add endpoint

--- packages/functions/src/services/HelloWorldService.ts
+++ packages/functions/src/services/HelloWorldService.ts
@@ -1,5 +1,9 @@
 import * as functions from 'firebase-functions'
 
 export const helloWorld = functions.https.onRequest(async (req, res) => {
-  res.json('ping')
+  res.send('hello world!')
+})
+
+export const ping = functions.https.onRequest(async (req, res) => {
+  res.send('pong')
 })
