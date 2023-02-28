51313d4033cf05bc70682bdaa345b6ce339d5f59
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-02-28 12:42:27 +0900

    FirebaseApp作成

--- packages/client/src/libs/FirebaseApp.ts
+++ packages/client/src/libs/FirebaseApp.ts
@@ -5,14 +5,14 @@ export const getFirebaseApp: () => Promise<FirebaseApp> =
   async () => {
     return fb.getApps().length === 0
       ? fb.initializeApp({
-        apiKey: process.env.VITE_FIREBASE_API_KEY,
-        authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-        projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.VITE_FIREBASE_APP_ID,
-        measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
       })
       : fb.getApps()[0]
   }
