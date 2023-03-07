57297fba03d41c8317c5031bf3cfd6f002cb18ee
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-03-07 22:21:48 +0900

    wip

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,9 +1,7 @@
 import * as HelloWorldService from './services/HelloWorldService'
 import * as MailService from './services/MailService'
-import * as ApplicationService from './services/ApplicationService'
 
 export default {
   HelloWorldService,
-  MailService,
-  ApplicationService
+  MailService
 }
