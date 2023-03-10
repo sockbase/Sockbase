53caa0c95a8253eeda331eaf347fc168093f6196
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-03-10 15:26:18 +0900

    テストデータ入力ボタン 環境別表示制御

--- packages/client/src/components/pages/events/Application/Step1.tsx
+++ packages/client/src/components/pages/events/Application/Step1.tsx
@@ -247,11 +247,13 @@ const Step1: React.FC<Props> = (props) => {
 
   return (
     <>
-      <FormSection>
-        <FormItem>
-          <FormButton onClick={setTestData} color="info">テストデータ入力(開発用)</FormButton>
-        </FormItem>
-      </FormSection>
+      {import.meta.env.DEV &&
+        <FormSection>
+          <FormItem>
+            <FormButton onClick={setTestData} color="info">テストデータ入力(開発用)</FormButton>
+          </FormItem>
+        </FormSection>
+      }
       <FormSection>
         <FormItem>
           <FormButton color="default" onClick={props.prevStep}>申し込み説明画面へ戻る</FormButton>
