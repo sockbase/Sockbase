12d00114ffef8d1b632ce0f457e2f7ea7dbfe69e
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-05-24 01:06:34 +0900

    決済リファクタリング

--- packages/functions/src/services/CheckoutWebhookService.ts
+++ packages/functions/src/services/CheckoutWebhookService.ts
@@ -1,8 +1,8 @@
 import * as functions from 'firebase-functions'
 import { Stripe } from 'stripe'
-import FirebaseAdmin from '../libs/FirebaseAdmin'
 import { paymentConverter, userConverter } from '../libs/converters'
-import type * as types from 'packages/types/src'
+import type * as types from 'sockbase'
+import firebaseAdmin from '../libs/FirebaseAdmin'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', { apiVersion: '2022-11-15' })
 
@@ -12,38 +12,48 @@ enum Status {
   PaymentFailure = 3
 }
 
-const firestore = FirebaseAdmin.getFirebaseAdmin().firestore()
+const firestore = firebaseAdmin.getFirebaseAdmin().firestore()
 
 const updateStatus = async (
   payments: types.SockbasePaymentDocument[],
-  items: Stripe.LineItem[] | undefined,
-  status: number,
+  items: Stripe.LineItem[],
+  status: types.PaymentStatus,
   now: Date
 ): Promise<void> => {
-  const itemIds = (items ?? []).filter(x => x.price !== undefined).map(x => x.price?.id as string)
-  for (const payment of payments) {
-    if (itemIds.includes(payment.paymentProductId)) {
-      await firestore.doc(`/_payments/${payment.id}`).set({
-        status,
-        updatedAt: now
-      }, { merge: true })
-    }
-  }
+  const itemIds = items
+    .filter(x => !x.price)
+    .map(x => x.price?.id)
+
+  const payment = payments
+    .filter(p => itemIds.includes(p.paymentProductId))[0]
+
+  await firestore.doc(`/_payments/${payment.id}`)
+    .set({
+      status,
+      updateStatus: now
+    }, { merge: true })
 }
 
 const collectPayments = async (userId: string, status: number): Promise<types.SockbasePaymentDocument[]> => {
   const paymentSnapshot = await firestore.collection('_payments')
+    .withConverter(paymentConverter)
     .where('userId', '==', userId)
     .where('status', '==', status)
-    .withConverter(paymentConverter)
     .get()
-  return paymentSnapshot.docs.map(x => x.data())
+
+  return paymentSnapshot.docs
+    .map(x => x.data())
 }
 
 const getUser = async (email: string): Promise<types.SockbaseAccountDocument | null> => {
-  const userSnapshot = await firestore.collection('users').where('email', '==', email).withConverter(userConverter).get()
-  const users = userSnapshot.docs.map(x => x.data())
-  return users.length === 0 ? null : users[0]
+  const userSnapshot = await firestore.collection('users')
+    .withConverter(userConverter)
+    .where('email', '==', email)
+    .get()
+  const users = userSnapshot.docs
+    .map(x => x.data())
+
+  return users.length === 1 ? users[0] : null
 }
 
 const HANDLEABLE_EVENTS = {
@@ -54,36 +64,41 @@ const HANDLEABLE_EVENTS = {
 
 export const treatCheckoutStatusWebhook = functions.https.onRequest(async (req, res) => {
   const event = req.body
-  // 扱うイベント以外は即returnする
+
   if (!Object.values(HANDLEABLE_EVENTS).includes(event.type)) {
     res.send({})
     return
   }
+
   const now = new Date()
+
   const session = event.data.object as Stripe.Checkout.Session
-  const lineItems = await stripe.checkout.sessions.listLineItems(session.id)
-  if (session.customer_details === null) {
+  if (!session.customer_details) {
     res.status(404).send({ error: 'NotFound', detail: 'custome is missing' })
     return
   }
+
   const email = session.customer_details.email
-  if (email === null) {
+  if (!email) {
     res.status(400).send({ error: 'EmailIsMissing', detail: 'email is missing' })
     return
   }
+
   const user = await getUser(email)
-  if (user === null) {
+  if (!user) {
     res.status(404).send({ error: 'NotFound', detail: `user(${email}) is not found` })
     return
   }
+
+  const lineItems = await stripe.checkout.sessions.listLineItems(session.id)
+
   switch (event.type) {
     case HANDLEABLE_EVENTS.checkoutCompleted: {
-      const payments = await collectPayments(user.id, Status.Pending)
       // クレカなどの即決済時のみ処理する
       // 銀行振り込みなどの遅延決済時はなにも処理しない
-      if (session.payment_status !== 'paid') {
-        break
-      }
+      if (session.payment_status !== 'paid') break
+
+      const payments = await collectPayments(user.id, Status.Pending)
       await updateStatus(payments, lineItems.data, Status.Paid, now)
       break
     }
@@ -100,5 +115,6 @@ export const treatCheckoutStatusWebhook = functions.https.onRequest(async (req, 
       break
     }
   }
+
   res.send({})
 })
