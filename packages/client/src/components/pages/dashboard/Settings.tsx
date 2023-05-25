5e822a58d4c0de0940075f04932abcac257e2706
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-05-25 21:58:13 +0900

    ユーザデータ読み込み

--- packages/client/src/components/pages/dashboard/Settings.tsx
+++ packages/client/src/components/pages/dashboard/Settings.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
+import type { SockbaseAccount } from 'sockbase'
+
 import { MdSettings } from 'react-icons/md'
-import PageTitle from '../../Layout/Dashboard/PageTitle'
+
 import TwoColumnsLayout from '../../Layout/TwoColumns/TwoColumns'
 
-const DashboardSettings: React.FC = () => {
+import PageTitle from '../../Layout/Dashboard/PageTitle'
+import FormSection from '../../Form/FormSection'
+import FormItem from '../../Form/FormItem'
+import FormLabel from '../../Form/Label'
+import FormInput from '../../Form/Input'
+import FormButton from '../../Form/Button'
+
+interface Props {
+  userData: SockbaseAccount
+  updateUserData: () => void
+}
+const DashboardSettings: React.FC<Props> = (props) => {
+  const [userData, setUserData] = useState<SockbaseAccount>()
+
+  const onInitialize: () => void =
+    () => {
+      if (!props.userData) return
+      setUserData(props.userData)
+    }
+  useEffect(onInitialize, [props.userData])
+
   return (
     <>
       <PageTitle
@@ -12,18 +35,53 @@ const DashboardSettings: React.FC = () => {
 
       <TwoColumnsLayout>
         <>
-          <h2>ログイン情報</h2>
-          <h3>メールアドレス</h3>
-          <h3>パスワード</h3>
-          <p>
-            メールアドレスの変更・パスワードの再設定は現在準備中です。<br />
-            再設定を希望される方は「お問い合わせ」よりご連絡ください。
-          </p>
-          <h2>個人情報</h2>
+          <FormSection>
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormInput value={userData?.email} />
+            </FormItem>
+            <FormItem>
+              <FormLabel>氏名</FormLabel>
+              <FormInput value={userData?.name} />
+            </FormItem>
+            <FormItem>
+              <FormLabel>誕生日</FormLabel>
+              <FormInput value={userData?.email} />
+            </FormItem>
+            <FormItem>
+              <FormLabel>郵便番号</FormLabel>
+              <FormInput value={userData?.postalCode} />
+            </FormItem>
+            <FormItem>
+              <FormLabel>住所</FormLabel>
+              <FormInput value={userData?.address} />
+            </FormItem>
+            <FormItem>
+              <FormLabel>電話番号</FormLabel>
+              <FormInput value={userData?.telephone} />
+            </FormItem>
+          </FormSection>
+          <FormSection>
+            <FormItem>
+              <FormButton onClick={() => props.updateUserData()}>保存</FormButton>
+            </FormItem>
+          </FormSection>
         </>
+
         <>
           <h2>お問い合わせ</h2>
+
+          <h3>パスワード</h3>
+          <p>
+            パスワードの再設定は現在準備中です。<br />
+            再設定を希望される方は「お問い合わせ」よりご連絡ください。
+          </p>
+
           <h3>アカウント削除</h3>
+          <p>
+            アカウント削除は現在準備中です。<br />
+            削除を希望される方は「お問い合わせ」よりご連絡ください。
+          </p>
         </>
       </TwoColumnsLayout>
     </>
--- packages/client/src/pages/dashboard/Settings.tsx
+++ packages/client/src/pages/dashboard/Settings.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from 'react'
+import type { SockbaseAccount } from 'sockbase'
+
+import useUserData from '../../hooks/useUserData'
+
 import DashboardLayout from '../../components/Layout/Dashboard/Dashboard'
 import DashboardSettings from '../../components/pages/dashboard/Settings'
 
 const DashboardContainer: React.FC = () => {
+  const { getMyUserDataAsync } = useUserData()
+
+  const [userData, setUserData] = useState<SockbaseAccount>()
+
+  const updateUserData: () => void =
+    () => {
+      alert('hello world!')
+    }
+
+  const onInitialize: () => void =
+    () => {
+      const fetchUserDataAsync: () => Promise<void> =
+        async () => {
+          const fetchedUserData = await getMyUserDataAsync()
+          if (!fetchedUserData) return
+          setUserData(fetchedUserData)
+        }
+      fetchUserDataAsync()
+        .catch(err => { throw err })
+    }
+  useEffect(onInitialize, [getMyUserDataAsync])
+
   return (
     <DashboardLayout title="マイページ設定">
-      <DashboardSettings />
+      {userData && <DashboardSettings
+        userData={userData}
+        updateUserData={updateUserData}
+      />}
     </DashboardLayout>
   )
 }
