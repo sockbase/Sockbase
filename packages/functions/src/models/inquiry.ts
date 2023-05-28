9112280a6afee1e8d93b3f5240d7889d865b6266
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-05-29 01:42:10 +0900

    問い合わせUX向上

--- packages/functions/src/models/inquiry.ts
+++ packages/functions/src/models/inquiry.ts
@@ -1,6 +1,14 @@
 export const convertTypeText: (type: string) => string =
   (type) => {
-    if (type === 'other') {
+    if (type === 'changeCircleInfo') {
+      return 'サークル申し込み情報変更'
+    } else if (type === 'payment') {
+      return '決済に関する相談'
+    } else if (type === 'changePassword') {
+      return 'パスワード再設定依頼'
+    } else if (type === 'removeAccount') {
+      return 'アカウント消去依頼'
+    } else if (type === 'other') {
       return 'その他'
     } else {
       return type
