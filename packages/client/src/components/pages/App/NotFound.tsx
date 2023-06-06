42a87b55b4e2842984d0c8e8e6a6ce3ccc273349
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-06-06 13:31:58 +0900

    トップへ戻る

--- packages/client/src/components/pages/App/NotFound.tsx
+++ packages/client/src/components/pages/App/NotFound.tsx
@@ -1,7 +1,42 @@
+import styled from 'styled-components'
+
+import FormSection from '../../Form/FormSection'
+import FormItem from '../../Form/FormItem'
+import LinkButton from '../../Parts/LinkButton'
+
 const NotFound: React.FC = () => {
   return (
-    <>404</>
+    <>
+      <StatusCodeHeader>
+        404
+      </StatusCodeHeader>
+      <StatusDescription>
+        ページが見つかりませんでした
+      </StatusDescription>
+      <FormSection>
+        <FormItem>
+          <LinkButton to="/" color="default">トップへ戻る</LinkButton>
+        </FormItem>
+      </FormSection>
+    </>
   )
 }
 
 export default NotFound
+
+const StatusCodeHeader = styled.section`
+  font-size: 10em;
+  font-weight: bold;
+  text-align: center;
+  color: var(--primary-color);
+
+  @media screen and (max-width: 840px) {
+    font-size: 5em;
+  }
+`
+
+const StatusDescription = styled.p`
+  font-size: 2em;
+  text-align: center;
+  color: #404040;
+`
