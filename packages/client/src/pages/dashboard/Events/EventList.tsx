5b331f53ebc34de2299d96d8b75188fc48af2678
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-07-23 20:25:10 +0900

    遅延ロード関連UX改善, 配置作成支援

--- packages/client/src/pages/dashboard/Events/EventList.tsx
+++ packages/client/src/pages/dashboard/Events/EventList.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom'
 import DashboardLayout from '../../../components/Layout/Dashboard/Dashboard'
+import Breadcrumbs from '../../../components/Parts/Breadcrumbs'
 import DashboardEventList from '../../../components/pages/dashboard/Events/EventList'
+import PageTitle from '../../../components/Layout/Dashboard/PageTitle'
+import { MdTableChart } from 'react-icons/md'
 
 const DashboardEventListContainer: React.FC = () => {
   return (
     <DashboardLayout title="管理イベント一覧">
+      <Breadcrumbs>
+        <li><Link to="/dashboard">マイページ</Link></li>
+      </Breadcrumbs>
+      <PageTitle
+        icon={<MdTableChart />}
+        title="管理イベント"
+        description="管理中のイベント一覧" />
+
       <DashboardEventList />
     </DashboardLayout>
   )
