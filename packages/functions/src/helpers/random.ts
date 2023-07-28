86229b909acd0edd83450aa57af2395e5899931c
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-07-28 22:20:50 +0900

    チケットIDの発行アルゴリズムを変更

---
+++ packages/functions/src/helpers/random.ts
@@ -0,0 +1,19 @@
+const generateRandomCharacters = (length: number): string => {
+  const BaseCharacters = '123456789ABCEFGHJKLNPRSTUWXYZabcdefghkmnprstvwxz'
+  const CharactersLength = BaseCharacters.length
+
+  const randoms: number[] = []
+  for (let i = 0; i < length; i++) {
+    randoms.push(Math.round(Math.random() * CharactersLength))
+  }
+
+  const characters = randoms
+    .map(r => BaseCharacters[r])
+    .join('')
+
+  return characters
+}
+
+export default {
+  generateRandomCharacters
+}
