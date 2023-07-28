24ec775d676c188dc518ee05031dbb1e9b43849d
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-07-28 21:36:43 +0900

    チケット割り当て仮実装

--- packages/client/src/components/pages/public/TicketView/TicketViewPanel.tsx
+++ packages/client/src/components/pages/public/TicketView/TicketViewPanel.tsx
@@ -1,11 +1,20 @@
-import styled from 'styled-components'
-import { type SockbaseStoreDocument, type SockbaseTicketUserDocument } from 'sockbase'
-import ReactQRCode from 'react-qr-code'
 import { useMemo } from 'react'
+import styled from 'styled-components'
+import ReactQRCode from 'react-qr-code'
+import { type SockbaseStoreDocument, type SockbaseTicketUserDocument } from 'sockbase'
+
+import logotypeSVG from '../../../../assets/logotype.svg'
+import Alert from '../../../Parts/Alert'
+import { Link } from 'react-router-dom'
+import FormSection from '../../../Form/FormSection'
+import FormItem from '../../../Form/FormItem'
+import FormButton from '../../../Form/Button'
 
 interface Props {
+  ticketHashId: string
   ticketUser: SockbaseTicketUserDocument
   store: SockbaseStoreDocument
+  userId: string | null
 }
 const TicketViewPanel: React.FC<Props> = (props) => {
   const typeName = useMemo(() => {
@@ -27,13 +36,41 @@ const TicketViewPanel: React.FC<Props> = (props) => {
           </QRCodeArea>
         </TitleContainer>
         <ContentContainer>
-          <p>
-            上のQRコードを入口スタッフまでご提示ください。
-          </p>
-          <p>
-            QRコードを提示できない場合は、この画面を印刷しご持参ください。
-          </p>
+          {props.ticketUser.usableUserId === null
+            ? <>
+              <Alert title="チケットの割り当てが完了していません" type="danger">
+                チケットを使うユーザの割り当てが完了していません。
+                {props.userId !== props.ticketUser.userId
+                  && <>
+                    <br />
+                    チケット購入者からチケット受け取りURLを送付してもらい、情報を入力してください。
+                  </>}
+              </Alert>
+              {props.userId === props.ticketUser.userId
+                && <>
+                  <p>
+                    自分でこのチケットを使う場合は「チケットを有効化する」を押してください。<br />
+                    他の方にチケットを渡す場合は、<Link to={`/dashboard/tickets/${props.ticketHashId}`}>チケット情報表示ページ</Link> から チケット受け取りURL を渡してください。
+                  </p>
+                  <FormSection>
+                    <FormItem>
+                      <FormButton>チケットを有効化する</FormButton>
+                    </FormItem>
+                  </FormSection>
+                </>}
+            </>
+            : <>
+              <p>
+                上のQRコードを入口スタッフまでご提示ください。
+              </p>
+              <p>
+                QRコードを提示できない場合は、この画面を印刷しご持参ください。
+              </p>
+            </>}
         </ContentContainer>
+        <Footer>
+          <Logotype src={logotypeSVG} />
+        </Footer>
       </TicketContainer>
     </Container>
   )
@@ -44,19 +81,30 @@ export default TicketViewPanel
 const Container = styled.div`
   height: 100%;
   padding: 40px 25%;
-  background-color: #f0f0f0;
 
   @media screen and (max-width: 840px) {
     padding: 0;
   }
 `
 const TicketContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto auto auto;
   max-width: 840px;
+  
+  @media screen and (max-width: 840px) {
+    height: 100%;
+    grid-template-rows: auto 1fr auto;
+  }
 `
 const TitleContainer = styled.section<{ color?: string }>`
   padding: 20px;
-  background-color: ${props => props.color ?? '#c0c0c0'};
+  background-color: ${props => props.color ?? '#202020'};
+  color: #ffffff;
   text-align: center;
+  border-radius: 5px 5px 0 0;
+  @media screen and (max-width: 840px) {
+    border-radius: 0;
+  }
 `
 const StoreName = styled.div``
 const TypeName = styled.div`
@@ -73,5 +121,18 @@ const QRCode = styled(ReactQRCode)`
 const ContentContainer = styled.section`
   padding: 20px;
   background-color:#ffffff;
-  border-bottom: 10px solid var(--primary-color);
+`
+const Footer = styled.footer`
+  padding: 10px;
+  padding: calc(10px + env(safe-area-inset-bottom));
+  background-color: #c0c0c0;
+  text-align: right;
+  border-radius: 0 0 5px 5px;
+
+  @media screen and (max-width: 840px) {
+    border-radius: 0;
+  }
+`
+const Logotype = styled.img`
+  height: 16px;
 `
--- packages/client/src/pages/public/TicketView.tsx
+++ packages/client/src/pages/public/TicketView.tsx
@@ -3,13 +3,16 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { type SockbaseStoreDocument, type SockbaseTicketUserDocument } from 'sockbase'
 import TicketViewPanel from '../../components/pages/public/TicketView/TicketViewPanel'
-import useStore from '../../hooks/useStore'
 import Loading from '../../components/Parts/Loading'
 import TicketLayout from '../../components/Layout/Ticket/Ticket'
+import Alert from '../../components/Parts/Alert'
+import useStore from '../../hooks/useStore'
+import useFirebase from '../../hooks/useFirebase'
 
 const TicketView: React.FC = () => {
   const { ticketHashId } = useParams<{ ticketHashId: string }>()
   const { getTicketUserByHashIdOptionalAsync, getStoreByIdAsync } = useStore()
+  const { user } = useFirebase()
 
   const [ticketUser, setTicketUser] = useState<SockbaseTicketUserDocument | null>()
   const [store, setStore] = useState<SockbaseStoreDocument>()
@@ -18,11 +21,11 @@ const TicketView: React.FC = () => {
     const fetchAsync = async (): Promise<void> => {
       if (!ticketHashId) return
 
-      const ticketUser = await getTicketUserByHashIdOptionalAsync(ticketHashId)
-      setTicketUser(ticketUser)
-      if (!ticketUser) return
+      const fetchedTicketUser = await getTicketUserByHashIdOptionalAsync(ticketHashId)
+      setTicketUser(fetchedTicketUser)
+      if (!fetchedTicketUser) return
 
-      getStoreByIdAsync(ticketUser.storeId)
+      getStoreByIdAsync(fetchedTicketUser.storeId)
         .then(fetchedStore => setStore(fetchedStore))
         .catch(err => { throw err })
     }
@@ -34,16 +37,31 @@ const TicketView: React.FC = () => {
 
   return (
     <TicketLayout ticketUser={ticketUser} store={store}>
-      {(!ticketUser || !store) && <LoadingWrapper>
-        <Loading text="チケット情報" />
-      </LoadingWrapper>}
-      {ticketUser && store && <TicketViewPanel ticketUser={ticketUser} store={store} />}
-    </TicketLayout>
+      {(!ticketUser || !store)
+        && <LoadingContainer>
+          <Loading text="チケット情報" />
+          {ticketUser === null
+            && <Alert title="チケットの取得に失敗しました">
+              チケットの取得に失敗しました
+            </Alert>}
+        </LoadingContainer>}
+      {ticketHashId && ticketUser && store && user !== undefined
+        && <TicketViewPanel
+          ticketHashId={ticketHashId}
+          ticketUser={ticketUser}
+          store={store}
+          userId={user?.uid ?? null} />}
+    </TicketLayout >
   )
 }
 
 export default TicketView
 
-const LoadingWrapper = styled.div`
-  padding: 10px;
+const LoadingContainer = styled.div`
+  padding: 40px 25%;
+  height: 100%;
+  @media screen and (max-width: 840px) {
+    padding: 20px;
+  }
+  background-color: #ffffff;
 `
