2625b96592e40bbc2253471849eb05c74d919f93
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-10-07 21:35:44 +0900

    converterかませる

--- packages/functions/src/models/user.ts
+++ packages/functions/src/models/user.ts
@@ -1,16 +1,20 @@
 import type { SockbaseAccountDocument } from 'sockbase'
 import FirebaseAdmin from '../libs/FirebaseAdmin'
+import { userConverter } from '../libs/converters'
 
 const adminApp = FirebaseAdmin.getFirebaseAdmin()
 const firestore = adminApp.firestore()
 
 export const getUserDataAsync: (userId: string) => Promise<SockbaseAccountDocument> =
   async (userId: string) => {
-    const userDoc = await firestore.doc(`/users/${userId}`).get()
+    const userDoc = await firestore
+      .doc(`/users/${userId}`)
+      .withConverter(userConverter)
+      .get()
     const user = userDoc.data()
     if (!user) {
       throw new Error(`user not found (${userId})`)
     }
 
-    return user as SockbaseAccountDocument
+    return user
   }
