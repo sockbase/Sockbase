ed22c3d00d36322817c64343745fbe3268e87620
SAIGUSA Tomotada <ts@nectarition.jp>
2023-12-08 19:11:05 +0900

    権限周り修正

--- packages/client/src/pages/DashboardEvents/EventList.tsx
+++ packages/client/src/pages/DashboardEvents/EventList.tsx
@@ -7,6 +7,8 @@ const EventList: React.FC = () => {
         <li>ねくたりしょん
           <ul>
             <li><Link to="/dashboard/events/shiobana2">第二回しおばな祭</Link></li>
+            <li><Link to="/dashboard/events/adaichi1">足立の一歩</Link></li>
+            <li><Link to="/dashboard/events/futaste1">ふたばすてっぷ</Link></li>
           </ul>
         </li>
       </ul>
