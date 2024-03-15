aa99163050adf8efc7e4116b59975fa0aa8a9d9b
SAIGUSA Tomotada <ts@nectarition.jp>
2024-03-15 15:40:13 +0900

    別組織Stripe対応

--- packages/functions/src/controllers/CheckoutController.ts
+++ packages/functions/src/controllers/CheckoutController.ts
@@ -1,6 +1,7 @@
 import { https } from 'firebase-functions'
 import CheckoutService from '../services/CheckoutService'
 
-export const treatCheckoutStatusWebhook = https.onRequest(async (req, res) =>
-  await CheckoutService.checkoutPaymentAsync(req, res)
-)
+export const treatCheckoutStatusWebhook = https
+  .onRequest(async (req, res) =>
+    await CheckoutService.checkoutPaymentAsync(req, res)
+  )
