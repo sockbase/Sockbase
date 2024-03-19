00eaaf05246a28f7f8a273a40085c8cf2c6de62d
SAIGUSA Tomotada <ts@nectarition.jp>
2024-03-19 19:57:20 +0900

    配置ソート機能

--- packages/client/src/pages/DashboardEvents/EventCircleApplications.tsx
+++ packages/client/src/pages/DashboardEvents/EventCircleApplications.tsx
@@ -1,5 +1,6 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
+import SortButton from '../../components/Parts/SortButton'
 import ApplicationStatusLabel from '../../components/Parts/StatusLabel/ApplicationStatusLabel'
 import type {
   SockbaseApplicationDocument,
@@ -19,12 +20,32 @@ interface Props {
   spaces: SockbaseSpaceDocument[]
 }
 const EventCircleApplications: React.FC<Props> = (props) => {
+  const [activeSortSpace, setActiveSortSpace] = useState(false)
+
   const circleCount = useMemo(() => {
     const appCount = Object.keys(props.apps)
       .filter(i => props.metas[i].applicationStatus !== 1)
       .length
     return appCount
   }, [props.apps])
+
+  const sortedApps = useMemo(() =>
+    Object.entries(props.apps)
+      .filter(([_, app]) => !!app.hashId)
+      .sort(([_a, a], [_b, b]) => (b.createdAt?.getTime() ?? 9) - (a.createdAt?.getTime() ?? 0))
+      .sort(([_aAppId, a], [_bAppId, b]): number => {
+        if (activeSortSpace) {
+          const aAppHash = props.appHashs.filter(app => app.applicationId === _aAppId)[0]
+          const bAppHash = props.appHashs.filter(app => app.applicationId === _bAppId)[0]
+          if (!aAppHash || !bAppHash || !aAppHash.spaceId || !bAppHash.spaceId) return 0
+
+          const aSpace = props.spaces.filter(s => s.id === aAppHash.spaceId)[0]
+          const bSpace = props.spaces.filter(s => s.id === bAppHash.spaceId)[0]
+          return (aSpace.spaceGroupOrder * 100 + aSpace.spaceOrder) - (bSpace.spaceGroupOrder * 100 + bSpace.spaceOrder)
+        } else {
+          return 0
+        }
+      }), [props.appHashs, props.spaces, activeSortSpace])
 
   const getSpace = useCallback((spaceId: string): SockbaseEventSpace | null => {
     if (!props.event) return null
@@ -68,7 +89,7 @@ const EventCircleApplications: React.FC<Props> = (props) => {
             <th>#</th>
             <th></th>
             <th>申し込みID</th>
-            <th>配置</th>
+            <th><SortButton active={activeSortSpace} onClick={() => setActiveSortSpace(s => !s)}>配置</SortButton></th>
             <th>サークル名</th>
             <th>ペンネーム</th>
             <th>申し込みスペース</th>
@@ -77,23 +98,19 @@ const EventCircleApplications: React.FC<Props> = (props) => {
           </tr>
         </thead>
         <tbody>
-          {
-            Object.entries(props.apps)
-              .filter(([_, app]) => !!app.hashId)
-              .sort(([_a, a], [_b, b]) => (b.createdAt?.getTime() ?? 9) - (a.createdAt?.getTime() ?? 0))
-              .map(([appId, app], i) => (
-                app.hashId && <tr key={app.hashId}>
-                  <td>{Object.entries(props.apps).length - i}</td>
-                  <td><ApplicationStatusLabel status={props.metas[appId].applicationStatus} /></td>
-                  <td>{app.hashId}</td>
-                  <td>{getAssignedSpace(app.hashId)?.spaceName ?? '-'}</td>
-                  <th><Link to={`/dashboard/applications/${app.hashId}`}>{app.circle.name}</Link></th>
-                  <td>{app.circle.penName}</td>
-                  <td>{getSpace(app.spaceId)?.name}</td>
-                  <td>{getGenre(app.circle.genre)?.name}</td>
-                  <td>{(app.unionCircleId && getCircleByHashId(app.unionCircleId)?.circle.name) || '-'}</td>
-                </tr>
-              ))
+          {sortedApps.map(([appId, app], i) => (
+            app.hashId && <tr key={app.hashId}>
+              <td>{Object.entries(props.apps).length - i}</td>
+              <td><ApplicationStatusLabel status={props.metas[appId].applicationStatus} /></td>
+              <td>{app.hashId}</td>
+              <td>{getAssignedSpace(app.hashId)?.spaceName ?? '-'}</td>
+              <th><Link to={`/dashboard/applications/${app.hashId}`}>{app.circle.name}</Link></th>
+              <td>{app.circle.penName}</td>
+              <td>{getSpace(app.spaceId)?.name}</td>
+              <td>{getGenre(app.circle.genre)?.name}</td>
+              <td>{(app.unionCircleId && getCircleByHashId(app.unionCircleId)?.circle.name) || '-'}</td>
+            </tr>
+          ))
           }
         </tbody>
       </table>
