1bf31c16007cbfd039a83c83af35d38c24d2acd2
SAIGUSA Tomotada <ts@nectarition.jp>
2024-05-15 02:47:42 +0900

    エクセル出力

--- packages/client/src/pages/DashboardEventSpaces/StepContainer.tsx
+++ packages/client/src/pages/DashboardEventSpaces/StepContainer.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { MdDownload } from 'react-icons/md'
+import FormButton from '../../components/Form/Button'
+import FormItem from '../../components/Form/FormItem'
+import FormSection from '../../components/Form/FormSection'
+import IconLabel from '../../components/Parts/IconLabel'
+import useApplication from '../../hooks/useApplication'
 import useEvent from '../../hooks/useEvent'
 import SpaceAssign from './SpaceAssign'
 import SpaceAssignConfirm from './SpaceAssignConfirm'
@@ -6,15 +12,21 @@ import SpaceCreate from './SpaceCreate'
 import SpaceCreateConfirm from './SpaceCreateConfirm'
 import SpaceView from './SpaceView'
 import type { RawAssignEventSpace, RawEventSpace } from '../../@types'
-import type { SockbaseApplicationDocument, SockbaseSpaceDocument } from 'sockbase'
+import type { SockbaseAccount, SockbaseApplicationDocument, SockbaseApplicationLinksDocument, SockbaseApplicationMeta, SockbaseApplicationOverviewDocument, SockbaseEvent, SockbaseSpaceDocument } from 'sockbase'
 
 interface Props {
   eventId: string
+  event: SockbaseEvent
   spaces: SockbaseSpaceDocument[]
   apps: Record<string, SockbaseApplicationDocument>
+  metas: Record<string, SockbaseApplicationMeta>
+  users: Record<string, SockbaseAccount>
+  links: Record<string, SockbaseApplicationLinksDocument | null>
+  overviews: Record<string, SockbaseApplicationOverviewDocument | null>
 }
 const StepContainer: React.FC<Props> = (props) => {
   const { createSpacesAsync, assignSpacesAsync } = useEvent()
+  const { downloadXLSX } = useApplication()
 
   const [step, setStep] = useState(0)
   const [stepComponents, setStepComponents] = useState<React.ReactNode[]>([])
@@ -23,59 +35,84 @@ const StepContainer: React.FC<Props> = (props) => {
   const [spacesData, setSpacesData] = useState<SockbaseSpaceDocument[]>([])
   const [rawAssignSpaces, setRawAssignSpaces] = useState<RawAssignEventSpace[]>([])
 
-  const createSpaces = async (): Promise<void> => {
+  const createSpaces = useCallback(async () => {
     if (rawSpaces.length === 0) return
     await createSpacesAsync(props.eventId, rawSpaces)
       .then(fetchedSpacesData => setSpacesData(fetchedSpacesData))
       .catch(err => { throw err })
-  }
+  }, [rawSpaces, props.eventId])
 
-  const assignSpaces = async (): Promise<void> => {
+  const assignSpaces = useCallback(async () => {
     if (rawAssignSpaces.length === 0) return
     await assignSpacesAsync(rawAssignSpaces)
       .catch(err => { throw err })
-  }
+  }, [rawAssignSpaces])
 
-  const onInitialize = (): void => {
+  const handleDownloadXLSX = useCallback(() => {
+    downloadXLSX(
+      props.eventId,
+      props.event,
+      props.apps,
+      props.metas,
+      props.users,
+      props.links,
+      props.overviews)
+  }, [props])
+
+  useEffect(() => {
     const _spacesData = props.spaces.length > 0 ? props.spaces : spacesData
     if (props.spaces.length > 0 && step === 0) {
       setStep(2)
     }
 
     setStepComponents([
-      <SpaceCreate key="space-create"
+      <SpaceCreate
+        key="space-create"
         rawSpaces={rawSpaces}
         nextStep={(rawSpaces: RawEventSpace[]) => {
           setRawSpaces(rawSpaces)
           setStep(1)
         }} />,
-      <SpaceCreateConfirm key="space-create-confirm"
+      <SpaceCreateConfirm
+        key="space-create-confirm"
         rawSpaces={rawSpaces}
         createSpaces={createSpaces}
         prevStep={() => setStep(0)}
         nextStep={() => setStep(2)} />,
-      <SpaceAssign key="space-assign"
+      <SpaceAssign
+        key="space-assign"
         spacesData={_spacesData}
         rawAssignSpaces={rawAssignSpaces}
         nextStep={(rawSpaces: RawAssignEventSpace[]) => {
           setRawAssignSpaces(rawSpaces)
           setStep(3)
         }} />,
-      <SpaceAssignConfirm key="space-assign-confirm"
+      <SpaceAssignConfirm
+        key="space-assign-confirm"
         apps={props.apps}
         spacesData={_spacesData}
         rawAssignSpaces={rawAssignSpaces}
         handleSubmit={assignSpaces}
         prevStep={() => setStep(2)}
         nextStep={() => setStep(4)} />,
-      <SpaceView key="space-view" eventId={props.eventId}/>
+      <SpaceView
+        key="space-view"
+        eventId={props.eventId}/>
     ])
-  }
-  useEffect(onInitialize, [props.spaces, rawSpaces, spacesData, rawAssignSpaces, props.apps, props.eventId])
+  }, [props.spaces, rawSpaces, spacesData, rawAssignSpaces, props.apps, props.eventId])
 
   return (
     <>
       {stepComponents?.[step]}
+      <FormSection>
+        <FormItem inlined>
+          <FormButton onClick={handleDownloadXLSX} inlined>
+            <IconLabel
+              label="配置データをダウンロード"
+              icon={<MdDownload />} />
+          </FormButton>
+        </FormItem>
+      </FormSection>
     </>
   )
 }
