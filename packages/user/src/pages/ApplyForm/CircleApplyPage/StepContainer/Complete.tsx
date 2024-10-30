ceba967498f8a1c894109793c2ddeff344876e20
SAIGUSA Tomotada <ts@nectarition.jp>
2024-10-30 23:56:12 +0900

    お申し込みを受け付けました！

--- packages/user/src/pages/ApplyForm/CircleApplyPage/StepContainer/Complete.tsx
+++ packages/user/src/pages/ApplyForm/CircleApplyPage/StepContainer/Complete.tsx
@@ -1,4 +1,7 @@
+import { useMemo } from 'react'
 import { MdInfo, MdWeb } from 'react-icons/md'
+import styled from 'styled-components'
+import ApplicationCompleteImage from '../../../../assets/application-complete.png'
 import FormItem from '../../../../components/Form/FormItem'
 import FormSection from '../../../../components/Form/FormSection'
 import AnchorButton from '../../../../components/Parts/AnchorButton'
@@ -14,17 +17,30 @@ interface Props {
 const Complete: React.FC<Props> = (props) => {
   const { formatByDate } = useDayjs()
 
+  const tweetText = useMemo(() => {
+    if (!props.event) return
+    const postURL = 'https://twitter.com/intent/tweet?text='
+    const message = `「${props.event.name}」に申し込みました！\n${props.event.websiteURL}\n\n`
+    return `${postURL}${encodeURIComponent(message)}`
+  }, [props.event])
+
   return (
     <>
-      <h1>申し込みが完了しました</h1>
-      <p>
-        お申し込みいただきましてありがとうございました。
-      </p>
+      <ApplicationCompleteArea>
+        <ApplicationCompleteLogotype src={ApplicationCompleteImage} />
+        <p>
+          「{props.event?.name}」へのサークル申し込み手続きが完了しました。<br />
+          お申し込みいただきましてありがとうございました。<br />
+          <TweetButton href={tweetText} target="_blank" rel="noopener noreferrer">
+            Twitterでシェアする
+          </TweetButton>
+        </p>
+      </ApplicationCompleteArea>
 
       <h2>サークルカットの提出・差し替え</h2>
       <p>
         サークルカットの提出・差し替えは「申し込み内容確認ページ」から行うことができます。<br />
-        {formatByDate((props.event?.schedules.catalogInformationFixedAt ?? 0) - 1, 'YYYY年 M月 D日')}時点の情報を元にカタログ等を制作いたしますので、変更がある場合はこの日までにご提出いただくようお願いいたします。
+        <b>{formatByDate((props.event?.schedules.catalogInformationFixedAt ?? 0) - 1, 'YYYY年M月D日')}</b>時点の情報を元にカタログ等を制作いたしますので、変更がある場合はこの日までにご提出いただくようお願いいたします。
       </p>
 
       <FormSection>
@@ -44,3 +60,26 @@ const Complete: React.FC<Props> = (props) => {
 }
 
 export default Complete
+
+const ApplicationCompleteArea = styled.div`
+  margin-bottom: 20px;
+  text-align: center;
+`
+const ApplicationCompleteLogotype = styled.img`
+  margin-bottom: 10px;
+  max-width: 100%;
+  max-height: 96px;
+`
+const TweetButton = styled.a`
+  display: inline-block;
+  margin-top: 10px;
+  padding: 2px 20px;
+  background-color: #1da1f2;
+  color: white;
+  border-radius: 5px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #118be3;
+  }
+`
