cc4971cefc10379a5f8aca6460c271fc602dcc4f
SAIGUSA Tomotada <ts@nectarition.jp>
2024-11-19 19:47:40 +0900

    initializeAppCheck

--- packages/user/src/main.tsx
+++ packages/user/src/main.tsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client'
 import './styles/main.scss'
 
 import App from './App'
+import { getFirebaseApp, initializeAppCheck } from './libs/FirebaseApp'
+
+getFirebaseApp()
+initializeAppCheck()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
