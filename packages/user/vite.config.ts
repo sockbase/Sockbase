bae77baa14a52880124bd9978f24472e34482bae
SAIGUSA Tomotada <ts@nectarition.jp>
2024-11-19 19:04:23 +0900

    開発環境でのみ証明書を参照する

--- packages/user/vite.config.ts
+++ packages/user/vite.config.ts
@@ -1,16 +1,20 @@
 import fs from 'fs'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react()
-  ],
-  server: {
-    https: {
-      key: fs.readFileSync('../../certs/localhost-key.pem'),
-      cert: fs.readFileSync('../../certs/localhost.pem')
+export default ({ mode }: { mode: string }): UserConfig => {
+  return defineConfig({
+    plugins: [
+      react()
+    ],
+    server: {
+      ...mode !== 'production' && ({
+        https: {
+          key: fs.readFileSync('../../certs/localhost-key.pem'),
+          cert: fs.readFileSync('../../certs/localhost.pem')
+        }
+      })
     }
-  }
-})
+  })
+}
