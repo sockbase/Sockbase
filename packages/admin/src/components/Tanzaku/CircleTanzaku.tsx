21d1f5491724042fb05a9736a338de1af6b442c9
SAIGUSA Tomotada <ts@nectarition.jp>
2024-11-21 22:40:20 +0900

    短冊デザイン調整

--- packages/admin/src/components/Tanzaku/CircleTanzaku.tsx
+++ packages/admin/src/components/Tanzaku/CircleTanzaku.tsx
@@ -1,10 +1,8 @@
 import { useMemo } from 'react'
 import styled from 'styled-components'
 import useDayjs from '../../hooks/useDayjs'
-import B5Page from '../Print/A5Page'
-import PrintBlankBar from '../Print/PrintBlankBar'
+import A5Page from '../Print/A5Page'
 import PrintContainer from '../Print/PrintContainer'
-import PrintMainContent from '../Print/PrintMainContent'
 import PrintPlaceHolderField from '../Print/PrintPlaceHolderField'
 import PrintSection from '../Print/PrintSection'
 import PrintSectionTitle from '../Print/PrintSectionTitle'
@@ -50,209 +48,206 @@ const CircleTanzaku: React.FC<Props> = (props) => {
   }, [props.event, props.userData])
 
   return (
-    <B5Page>
+    <A5Page>
       <PrintContainer>
         <Header>
           {props.event._organization.name} {props.event.name} 配置短冊
         </Header>
         <Main>
-          <PrintMainContent>
-            <TwoColumnLayout>
-              <>
-                <PrintSection>
-                  <IndicatorRack>
-                    <Indicator $active={props.app.circle.hasAdult}>
-                      <IndicatorIcon>成人</IndicatorIcon>
-                    </Indicator>
-                    <Indicator $active={spaceType?.isDualSpace ?? false}>
-                      <IndicatorIcon>2sp.</IndicatorIcon>
-                    </Indicator>
-                    <Indicator $active={!!props.app.unionCircleId}>
-                      <IndicatorIcon>合体</IndicatorIcon>
-                    </Indicator>
-                    <Indicator $active={!!props.app.petitCode}>
-                      <IndicatorIcon>ﾌﾟﾁｵﾝﾘｰ</IndicatorIcon>
-                    </Indicator>
-                  </IndicatorRack>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <th>合体先</th>
-                        <td>
+          <TwoColumnLayout>
+            <>
+              <PrintSection>
+                <IndicatorRack>
+                  <Indicator $active={props.app.circle.hasAdult}>
+                    <IndicatorIcon>成人</IndicatorIcon>
+                  </Indicator>
+                  <Indicator $active={spaceType?.isDualSpace ?? false}>
+                    <IndicatorIcon>2sp.</IndicatorIcon>
+                  </Indicator>
+                  <Indicator $active={!!props.app.unionCircleId}>
+                    <IndicatorIcon>合体</IndicatorIcon>
+                  </Indicator>
+                  <Indicator $active={!!props.app.petitCode}>
+                    <IndicatorIcon>ﾌﾟﾁｵﾝﾘｰ</IndicatorIcon>
+                  </Indicator>
+                </IndicatorRack>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <th>合体先</th>
+                      <td>
+                        {
+                          props.app.unionCircleId
+                            ? props.unionApp?.circle.name || '(合体先が存在しない)'
+                            : '(空欄)'
+                        }
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>ﾌﾟﾁｵﾝﾘｰｺｰﾄﾞ</th>
+                      <td>{props.app.petitCode || '(空欄)'}</td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+              </PrintSection>
+            </>
+            <>
+              <PrintSection>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <td style={{ height: '15mm' }}>ブロック</td>
+                      <td style={{ height: '15mm' }}>スペース</td>
+                      <td style={{ height: '15mm' }}>a/b</td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <th>申し込みID</th>
+                      <td>{props.app.hashId}</td>
+                    </tr>
+                    <tr>
+                      <th>スペース</th>
+                      <td>{spaceType?.name}</td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+              </PrintSection>
+            </>
+          </TwoColumnLayout>
+          <TwoColumnLayout>
+            <>
+              <PrintSection>
+                <PrintSectionTitle>1. 基礎情報</PrintSectionTitle>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <th>サークル名</th>
+                      <td>{props.app.circle.name}</td>
+                    </tr>
+                    <tr>
+                      <th>よみ</th>
+                      <td>{props.app.circle.yomi}</td>
+                    </tr>
+                    <tr>
+                      <th>ペンネーム</th>
+                      <td>{props.app.circle.penName}</td>
+                    </tr>
+                    <tr>
+                      <th>よみ</th>
+                      <td>{props.app.circle.penNameYomi}</td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+              </PrintSection>
+              <PrintSection>
+                <PrintSectionTitle>2. カタログ掲載情報</PrintSectionTitle>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <th>X (Twitter)</th>
+                      <td>{props.appLink.twitterScreenName || '(空欄)'}</td>
+                    </tr>
+                    <tr>
+                      <th>pixiv</th>
+                      <td>{props.appLink.pixivUserId || '(空欄)'}</td>
+                    </tr>
+                    <tr>
+                      <th>Web</th>
+                      <td>{props.appLink.websiteURL || '(空欄)'}</td>
+                    </tr>
+                    <tr>
+                      <th>お品書き</th>
+                      <td>{props.appLink.menuURL || '(空欄)'}</td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+              </PrintSection>
+            </>
+            <>
+              <PrintSection>
+                <PrintSectionTitle>3. 頒布物情報</PrintSectionTitle>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <th>成人向け作品</th>
+                      <td>{props.app.circle.hasAdult ? '有り' : '無し'}</td>
+                    </tr>
+                    <tr>
+                      <th>ジャンル</th>
+                      <td>{genreType?.name}</td>
+                    </tr>
+                    <tr>
+                      <th>頒布物概要</th>
+                      <td>
+                        <HeightLimit>
+                          {props.overview?.description ?? props.app.overview.description}
+                        </HeightLimit>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>総搬入量</th>
+                      <td>
+                        <HeightLimit>
+                          {props.overview?.totalAmount ?? props.app.overview.totalAmount}
+                        </HeightLimit>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>ｻｰｸﾙｶｯﾄ</th>
+                      <td style={{ fontSize: '0' }}>
+                        <CircleCutArea>
                           {
-                            props.app.unionCircleId
-                              ? props.unionApp?.circle.name || '(合体先が存在しない)'
-                              : '(空欄)'
+                            props.circleCutURL
+                              ? <CircleCut src={props.circleCutURL} />
+                              : '(未提出)'
                           }
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>ﾌﾟﾁｵﾝﾘｰｺｰﾄﾞ</th>
-                        <td>{props.app.petitCode || '(空欄)'}</td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                </PrintSection>
-              </>
-              <>
-                <PrintSection>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <td style={{ height: '15mm' }}>ブロック</td>
-                        <td style={{ height: '15mm' }}>スペース</td>
-                        <td style={{ height: '15mm' }}>a/b</td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <th>申し込みID</th>
-                        <td>{props.app.hashId}</td>
-                      </tr>
-                      <tr>
-                        <th>スペース</th>
-                        <td>{spaceType?.name}</td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                </PrintSection>
-              </>
-            </TwoColumnLayout>
-            <TwoColumnLayout>
-              <>
-                <PrintSection>
-                  <PrintSectionTitle>1. 基礎情報</PrintSectionTitle>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <th>サークル名</th>
-                        <td>{props.app.circle.name}</td>
-                      </tr>
-                      <tr>
-                        <th>よみ</th>
-                        <td>{props.app.circle.yomi}</td>
-                      </tr>
-                      <tr>
-                        <th>ペンネーム</th>
-                        <td>{props.app.circle.penName}</td>
-                      </tr>
-                      <tr>
-                        <th>よみ</th>
-                        <td>{props.app.circle.penNameYomi}</td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                </PrintSection>
-                <PrintSection>
-                  <PrintSectionTitle>2. カタログ掲載情報</PrintSectionTitle>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <th>X (Twitter)</th>
-                        <td>{props.appLink.twitterScreenName || '(空欄)'}</td>
-                      </tr>
-                      <tr>
-                        <th>pixiv</th>
-                        <td>{props.appLink.pixivUserId || '(空欄)'}</td>
-                      </tr>
-                      <tr>
-                        <th>Web</th>
-                        <td>{props.appLink.websiteURL || '(空欄)'}</td>
-                      </tr>
-                      <tr>
-                        <th>お品書き</th>
-                        <td>{props.appLink.menuURL || '(空欄)'}</td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                </PrintSection>
-              </>
-              <>
-                <PrintSection>
-                  <PrintSectionTitle>3. 頒布物情報</PrintSectionTitle>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <th>成人向け作品</th>
-                        <td>{props.app.circle.hasAdult ? '有り' : '無し'}</td>
-                      </tr>
-                      <tr>
-                        <th>ジャンル</th>
-                        <td>{genreType?.name}</td>
-                      </tr>
-                      <tr>
-                        <th>頒布物概要</th>
-                        <td>
-                          <HeightLimit>
-                            {props.overview?.description ?? props.app.overview.description}
-                          </HeightLimit>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>総搬入量</th>
-                        <td>
-                          <HeightLimit>
-                            {props.overview?.totalAmount ?? props.app.overview.totalAmount}
-                          </HeightLimit>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>ｻｰｸﾙｶｯﾄ</th>
-                        <td style={{ fontSize: '0' }}>
-                          <CircleCutArea>
-                            {
-                              props.circleCutURL
-                                ? <CircleCut src={props.circleCutURL} />
-                                : '(未提出)'
-                            }
-                          </CircleCutArea>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                </PrintSection>
-              </>
-              <>
-                <PrintSection>
-                  <PrintSectionTitle>4. 配置考慮情報</PrintSectionTitle>
-                  <PrintTable>
-                    <tbody>
-                      <tr>
-                        <th>性別</th>
-                        <td>
-                          {
-                            !props.userData.gender
-                              ? '(空欄)'
-                              : props.userData.gender === 1
-                                ? '男性'
-                                : '女性'
-                          }
-                        </td>
-                        <th>開催時年齢</th>
-                        <td>{age}歳</td>
-                      </tr>
-                    </tbody>
-                  </PrintTable>
-                </PrintSection>
-              </>
-            </TwoColumnLayout>
-            <PrintSection>
-              <PrintSectionTitle>5. 通信欄</PrintSectionTitle>
-              <PrintPlaceHolderField>
-                {props.app.remarks || '(空欄)'}
-              </PrintPlaceHolderField>
-            </PrintSection>
-          </PrintMainContent>
-          <PrintBlankBar />
+                        </CircleCutArea>
+                      </td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+              </PrintSection>
+            </>
+            <>
+              <PrintSection>
+                <PrintSectionTitle>4. 配置考慮情報</PrintSectionTitle>
+                <PrintTable>
+                  <tbody>
+                    <tr>
+                      <th>性別</th>
+                      <td>
+                        {
+                          !props.userData.gender
+                            ? '(空欄)'
+                            : props.userData.gender === 1
+                              ? '男性'
+                              : '女性'
+                        }
+                      </td>
+                      <th>開催時年齢</th>
+                      <td>{age}歳</td>
+                    </tr>
+                  </tbody>
+                </PrintTable>
+              </PrintSection>
+            </>
+          </TwoColumnLayout>
+          <PrintSection>
+            <PrintSectionTitle>5. 通信欄</PrintSectionTitle>
+            <PrintPlaceHolderField style={{ maxHeight: '28mm', overflow: 'hidden' }}>
+              {props.app.remarks || 'これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄これは空欄です何がなんでも空欄'}
+            </PrintPlaceHolderField>
+          </PrintSection>
         </Main>
         <Footer>
           サークル: {props.appIndex + 1} / {props.confirmedAppCount}<br />
           {formatByDate(props.now, 'YYYY/MM/DD HH:mm:ss')}
         </Footer>
       </PrintContainer>
-    </B5Page>
+    </A5Page>
   )
 }
 
@@ -266,7 +261,7 @@ const IndicatorRack = styled.div`
   margin-bottom: 1mm;
   height: 15.25mm;
   display: flex;
-  border: 1px solid black;
+  border: 0.25mm solid black;
 `
 const Indicator = styled.div<{ $active: boolean }>`
   flex: 1;
