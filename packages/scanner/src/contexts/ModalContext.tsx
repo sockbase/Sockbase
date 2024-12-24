cb66ec0ba58a85a10dfcc776c27087cb2474cf7d
SAIGUSA Tomotada <ts@nectarition.jp>
2024-12-24 13:21:50 +0900

    モーダルを Enter, Escape で抜けられるように変更

--- packages/scanner/src/contexts/ModalContext.tsx
+++ packages/scanner/src/contexts/ModalContext.tsx
@@ -1,9 +1,6 @@
-import { createContext, useCallback, useState } from 'react'
-import { PiX } from 'react-icons/pi'
+import { createContext, useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
-import FormButton from '../components/Form/FormButton'
-import FormItem from '../components/Form/FormItem'
-import FormSection from '../components/Form/FormSection'
+import Modal from '../components/Parts/Modal'
 
 export interface ModalProps {
   title: string
@@ -41,6 +38,22 @@ export const ModalProvider: React.FC<Props> = props => {
     })
   }, [])
 
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      handleCancel()
+    }
+    else if (e.key === 'Enter') {
+      handleOK()
+    }
+  }, [handleOK, handleCancel])
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
   return (
     <ModalContext.Provider value={showModalAsync}>
       <Container>
@@ -62,43 +75,6 @@ const ModalContext = createContext(async (_: ModalProps) => {})
 
 export default ModalContext
 
-interface ModalControlProps {
-  modalProps: ModalProps
-  handleOK: () => void
-  handleCancel: () => void
-}
-const Modal: React.FC<ModalControlProps> = props => {
-  return (
-    <ModalContainer>
-      <ModalTitleWrap>
-        <ModalTitle>{props.modalProps.title}</ModalTitle>
-        <ModalClose>
-          <ModalCloseButton onClick={props.handleCancel}>
-            <PiX />
-          </ModalCloseButton>
-        </ModalClose>
-      </ModalTitleWrap>
-      <ModalBody>
-        <ModalContent>{props.modalProps.children}</ModalContent>
-        <ModalFooter>
-          <FormSection>
-            <FormItem $inlined>
-              <FormButton onClick={props.handleOK}>
-                {props.modalProps.type === 'confirm' ? 'はい' : 'OK'}
-              </FormButton>
-              {props.modalProps.type === 'confirm' && (
-                <FormButton onClick={props.handleCancel}>
-                  いいえ
-                </FormButton>
-              )}
-            </FormItem>
-          </FormSection>
-        </ModalFooter>
-      </ModalBody>
-    </ModalContainer>
-  )
-}
-
 const Container = styled.div`
   position: relative;
   width: 100%;
@@ -114,52 +90,4 @@ const ModalWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
-const ModalContainer = styled.div`
-  width: 90%;
-  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-`
-const ModalTitleWrap = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto;
-  background-color: #404040;
-  color: white;
-  border-radius: 10px 10px 0 0;
-`
-const ModalTitle = styled.div`
-  font-weight: bold;
-  padding: 10px;
-`
-const ModalClose = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-const ModalCloseButton = styled.button`
-  width: calc(28px + 10px);
-  height: calc(28px + 10px);
-  background-color: transparent;
-  border: none;
-  padding: 10px;
-  svg {
-    color: white;
-    width: 100%;
-    height: 100%;
-  }
-`
-const ModalBody = styled.div`
-  background-color: white;
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  border-radius: 0 0 10px 10px;
-`
-const ModalContent = styled.div`
-  min-height: 5em;
-  color: black;
-`
-const ModalFooter = styled.div`
-  text-align: right;
 `
