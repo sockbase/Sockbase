3694bd18cc8f4720f9457a42b16c81b288461f71
SAIGUSA Tomotada <ts@nectarition.jp>
2025-01-26 14:30:42 +0900

    text-top

--- packages/user/public/shared/sb-button.js
+++ packages/user/public/shared/sb-button.js
@@ -26,7 +26,7 @@ if (typeof document !== 'undefined') {
         content: '';
         width: 24px;
         height: 24px;
-        vertical-align: top;
+        vertical-align: text-top;
         margin-right: 5px;
         background-image: url('data:image/svg+xml,<%3Fxml version="1.0" encoding="UTF-8"%3F><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><defs><style> .cls-1 { fill: %23fff; } </style></defs><path class="cls-1" d="M463.3,181.81l19.83-102.1H68.49L0,432.29h512l-48.7-250.48ZM375.46,389.9H55.65L107.67,122.1h319.81l-52.02,267.8Z"/></svg>');
       }
